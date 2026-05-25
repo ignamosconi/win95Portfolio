0783cb1f9f4fa3fac80aa14e44c94bd50d6bcb6f
Ignacio <ignamosconi@gmail.com>
2026-05-25 00:14:51 -0300

    fixed all start menu items

--- src/components/WindowsShutdown.jsx
+++ src/components/WindowsShutdown.jsx
@@ -6,8 +6,8 @@ function WindowsShutdown() {
   return (
     <div className='shitdown_bg_container'>
         <img src={windowsshut} alt="windowsShutdown" />
-        <h1 className='text_1_shutdown'>Please wait while your computer  <br /> shut down.</h1>
-        <h1 className='text_2_shutdown'>It is now safe to turn off  <br /> your computer.</h1>
+        <h1 className='text_1_shutdown'>Please wait while your computer  <br /> shuts down.</h1>
+        <h1 className='text_2_shutdown'>  <br /> </h1>
            
     </div>
   )
