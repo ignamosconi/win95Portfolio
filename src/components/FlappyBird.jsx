4b3723f9aef2930cd68b09d6228fec7f9c208bb6
Ignacio <ignamosconi@gmail.com>
2026-05-30 23:39:45 -0300

    fixed focus problem on games

--- src/components/FlappyBird.jsx
+++ src/components/FlappyBird.jsx
@@ -1,10 +1,9 @@
 import UseContext from '../Context'
-import { useContext } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import Draggable from 'react-draggable'
 import flappyBirdIcon from '../assets/flappy_bird_icon.png'
 
 function FlappyBird() {
-
   const {
     themeDragBar,
     isTouchDevice,
@@ -14,6 +13,15 @@ function FlappyBird() {
     inlineStyle,
     FlappyBirdExpand,
   } = useContext(UseContext);
+
+  const [focused, setFocused] = useState(false);
+  const touchStartTime = useRef(null)
+
+  useEffect(() => {
+    if (!FlappyBirdExpand.focusItem) {
+      setFocused(false);
+    }
+  }, [FlappyBirdExpand.focusItem]);
 
   return (
     <Draggable
@@ -29,19 +37,15 @@ function FlappyBird() {
           height: window.innerWidth <= 450 ? '400px' : '512px'
         }}
       >
-
         <div
           className="folder_dragbar"
           style={{ background: FlappyBirdExpand.focusItem ? themeDragBar : '#808080' }}
         >
-
           <div className="folder_barname">
             <img src={flappyBirdIcon} alt="FlappyBird" />
             <span>Flappy Bird by Igna :) - ↑ to flap</span>
           </div>
-
           <div className="folder_barbtn">
-
             <div>
               <p
                 className='x'
@@ -49,13 +53,28 @@ function FlappyBird() {
                 onTouchEnd={() => deleteTap('FlappyBird')}
               >×</p>
             </div>
-
           </div>
         </div>
-
-        <div
-          className="game_content"
-        >
+        <div className="game_content" style={{position: 'relative'}}>
+          {isTouchDevice && !focused && (
+            <div
+              style={{
+                position: 'absolute',
+                top: 0, left: 0,
+                width: '100%', height: '100%',
+                zIndex: 1,
+              }}
+              onTouchStart={() => {
+                touchStartTime.current = Date.now();
+              }}
+              onTouchEnd={() => {
+                if (Date.now() - touchStartTime.current < 500) {
+                  handleSetFocusItemTrue('FlappyBird');
+                  setFocused(true);
+                }
+              }}
+            />
+          )}
           <iframe
             src="https://www.lexaloffle.com/bbs/widget.php?pid=flappico_bird"
             allowFullScreen
@@ -64,7 +83,6 @@ function FlappyBird() {
             style={{ display: 'block' }}
           />
         </div>
-
       </div>
     </Draggable>
   )
